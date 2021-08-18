35dd253380fb3640b85630617123720b62566b61
Tarcisio Ortega <tarcisiocaetano2009@gmail.com>
2021-08-18 09:31:57 -0300

    Exceptions

---
+++ AuthGG.Client/Exceptions/InvalidAuthDataException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tcortega.AuthGG.Client.Exceptions
+{
+    public class InvalidAuthDataException : Exception
+    {
+        public InvalidAuthDataException()
+            : base("Invalid auth data provided!")
+        {
+        }
+
+        public InvalidAuthDataException(string errorMessage)
+            : base(errorMessage)
+        {
+        }
+    }
+}
